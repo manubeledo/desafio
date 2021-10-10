38e408ec412817ddc9a091267483be9746bcca47
manubeledo <92060861+manubeledo@users.noreply.github.com>
2021-10-09 21:07:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
 const fs = require("fs");
-const Contenedor = require('./Contenedor');
 const express = require('express');
 const app = express();
 const PORT = 8080;
 const server = app.listen(PORT, () => {
     console.log(`Servidor funcionando en la URL http://localhost:${PORT}`);
 })
-
-let teclado = new Contenedor('teclado', 'https://spacegamer.com.ar/img/Public/1058-producto-1019-producto-teclado-hyperx-alloy-fps-pro-blue-mecanico-3-2424-101.jpg', 200);
-let monitor = new Contenedor('monitor', 'https://http2.mlstatic.com/D_NQ_NP_949745-MLA44337500995_122020-O.jpg', 350);
-let gabinete = new Contenedor('gabinete', 'https://eclypse.com.ar/wp-content/uploads/2020/12/c200.jpg', 50);
-
-async function save(){
-    try {
-        let datos = JSON.stringify(Contenedor.productos);
-        await fs.promises.writeFile('productos.txt', datos);
-        const contenido = await fs.promises.readFile('productos.txt', 'utf-8');
-        let datosnew = JSON.parse(contenido);
-        console.log('Se creo el archivo: productos.txt')
-        datosnew.forEach(elemento => console.log(`Se guardo un elemento con el id: ${elemento.id}`));
-    }
-    catch(err){
-        console.log('Hubo un error en save');
-    }
-}
-
-save()
 
 app.get('/', (req, res) => {
     res.status(200).send(`<!DOCTYPE html>
@@ -39,7 +18,7 @@ app.get('/', (req, res) => {
     <body>
         <h1>Desafio de la clase de servidor con express</h1>
         <button onclick="window.location.href='/productos'">productos</button>
-        <button onclick="window.location.href='/productos'">productorandom</button>
+        <button onclick="window.location.href='/productorandom'">productorandom</button>
     </body>
     </html>`)
 })
